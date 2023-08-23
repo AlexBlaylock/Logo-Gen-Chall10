491b6762ba7c2b6775a303bd0d0480a3b1f53044
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-08-23 13:39:40 -0500

    added inquirer prompts

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const Logo = require('./logo.js');
 
 inquirer.prompt([
     {
-
+        type: 'input',
+        message: 'Enter 3 characters',
+        name: 'text',
+    },
+    {
+        type: 'input',
+        message: 'Enter a color for your logo (keyword or hexadecimal)',
+        name: 'text',
+    },
+    {
+        type: 'checkbox',
+        message: 'Select a shape for your logo',
+        name: '',
     },
 ])
