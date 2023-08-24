73164d817932c3798858421f0304098add9c9df1
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-08-23 22:34:45 -0500

    added to shapes.js, index.js, need to get inquirer prompt to work right

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
-const Logo = require('./logo.js');
+const Logo = require('./lib/logo.js');
+const fs = require('fs');
 
 inquirer.prompt([
     {
@@ -9,12 +10,41 @@ inquirer.prompt([
     },
     {
         type: 'input',
-        message: 'Enter a color for your logo (keyword or hexadecimal)',
-        name: 'text',
+        message: "Enter a color for your logo's text (keyword or hexadecimal)",
+        name: 'textColor',
     },
     {
         type: 'checkbox',
         message: 'Select a shape for your logo',
-        name: '',
+        name: 'shape',
+        choices: [
+            {
+              name: 'Circle',
+            },
+            {
+              name: 'Square',
+            },
+            {
+              name: 'Triangle',
+            },
+        ]
+    },
+    {
+        type: 'input',
+        message: "Enter a color for your logo's shape (keyword or hexadecimal)",
+        name: 'shapeColor',
     },
 ])
+
+.then(answers => {
+    const logo = new Logo(
+        answers.text,
+        answers.textColor,
+        answers.shape,
+        answers.shapeColor
+    );
+
+    logo.generateSVG();
+
+    console.log("Generated logo.svg");
+});
